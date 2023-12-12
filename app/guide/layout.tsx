926510c68dda0e02ee8e8070d43bdb9d335c6925
dohyunjung <dohj@mwkorea.co.kr>
2023-12-12 16:48:57 +0900

    설정 되돌리기

--- app/guide/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-type LayoutProps = {
-  children: ReactNode;
-};
-
-export default function GuideLayout({ children }: LayoutProps) {
-  return <section>{children}</section>;
-}
--- app/preset/logi-guide-preset.ts
+++
@@ -1,9 +0,0 @@
-import { Preset, definePreset } from 'unocss';
-
-export default definePreset(() => {
-  return {
-    // name: 'logi-guide-preset',
-    // shortcuts: [{}],
-    // it supports most of the configuration you could have in the root config
-  };
-});
