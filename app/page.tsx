b5ca217a015be290a880ccdb90281bce29a89a84
dohyunjung <dohj@mwkorea.co.kr>
2023-12-14 13:07:11 +0900

    초기세팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,182 @@
-export default function Home() {
+// -----------------------------------------------
+// logisoft WEB OEM
+// 2023.12 ~ 2024.05
+// publishing Guide Index
+// 화면ID로 검색
+// -----------------------------------------------
+
+import Link from 'next/link';
+
+export default function Page() {
+  const datas = require('@/public/_dummy/guidelist.json');
+  const date = new Date();
+  const formatDate = (date: any) => {
+    const d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+    return [year, month, day].join('.');
+  };
+  const headerTitle = [
+    'ID',
+    'depth2',
+    'depth3',
+    'depth4',
+    'depth5',
+    'depth6',
+    'path',
+    'status',
+    'create',
+    'update',
+    'log',
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <button>Button</button>
-        <p className="...">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
+    <div className="bg-white">
+      <h1 className="m-4 text-gray text-2xl font-bold">로지소프트</h1>
+      {/* component button */}
+      <div className="flex items-center ml-4">
+        <button
+          type="button"
+          className="flex items-center justify-center px-3 py-1.5 mr-2 text-white text-sm rounded-md font-semibold border-0 bg-cyan-500 hover:bg-cyan-700"
+        >
+          컴포넌트
+        </button>
+        <button
+          type="button"
+          className="flex items-center justify-center px-3 py-1.5 mr-2 text-white text-sm rounded-md font-semibold border-0 bg-cyan-500 hover:bg-cyan-700"
+        >
+          레이어팝업
+        </button>
+        <button
+          type="button"
+          className="flex items-center justify-center px-3 py-1.5 mr-2 text-white text-sm rounded-md font-semibold border-0 bg-cyan-500 hover:bg-cyan-700"
+        >
+          기타
+        </button>
       </div>
-    </main>
+      {/* guide table */}
+      <div className="relative m-4">
+        {datas.map((data: any, index: any) => (
+          <div className="mb-6" key={index} id={`section-${index}`}>
+            <h2 className="m-0 p-2 pl-4 text-sm font-bold leading-5 color-white rounded-t-lg bg-gray-700">
+              {data.depth1}
+            </h2>
+            <table className="border border-solid border-gray-200 text-sm w-full">
+              <colgroup>
+                <col width="100px" />
+                <col width="100px" />
+                <col width="100px" />
+                <col width="100px" />
+                <col width="100px" />
+                <col width="100px" />
+                <col />
+                <col width="100px" />
+                <col width="100px" />
+                <col width="100px" />
+                <col width="300px" />
+              </colgroup>
+              <thead>
+                <tr className="text-xs font-bold border border-solid border-gray-200  bg-zinc-200">
+                  {headerTitle.map((title) => (
+                    <th className="p-1.5 border border-solid border-gray-200">
+                      {title}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {data.below &&
+                  data.below.map((subdata: any, index: any) => (
+                    <tr className="text-xs" key={index}>
+                      <td className="p-1.5 border border-solid border-gray-200">
+                        {subdata.id}
+                      </td>
+                      <td className="p-1.5 border border-solid border-gray-200">
+                        {subdata.depth2}
+                      </td>
+                      <td className="p-1.5 border border-solid border-gray-200">
+                        {subdata.depth3}
+                      </td>
+                      <td className="p-1.5 border border-solid border-gray-200">
+                        {subdata.depth4}
+                      </td>
+                      <td className="p-1.5 border border-solid border-gray-200">
+                        {subdata.depth5}
+                      </td>
+                      <td className="p-1.5 border border-solid border-gray-200">
+                        {subdata.depth6}
+                      </td>
+                      <td className="p-1.5 border border-solid border-gray-200">
+                        <Link href={subdata.path} target="_blank">
+                          {subdata.path}
+                        </Link>
+                      </td>
+                      <td className="p-1.5 text-center border border-solid border-gray-200 ">
+                        <div className="text-white font-bold">
+                          <span
+                            className={`
+                              p-1 rounded focus:outline-none
+                            ${
+                              subdata.status === '작업중'
+                                ? 'bg-blue-600'
+                                : subdata.status === '수정중'
+                                  ? 'bg-red-500'
+                                  : subdata.status === '작업완료'
+                                    ? 'bg-gray-400'
+                                    : ''
+                            }
+                            `}
+                          >
+                            {subdata.status}
+                          </span>
+                        </div>
+                      </td>
+                      <td className="p-1.5 border border-solid border-gray-200">
+                        {subdata.create}
+                      </td>
+                      <td className="p-1.5 border border-solid border-gray-200">
+                        {subdata.update}
+                      </td>
+                      <td className="p-1.5 border border-solid border-gray-200">
+                        <ul>
+                          {subdata.log.map((item: any, index: any) => {
+                            return (
+                              <li
+                                className={`
+                                ${
+                                  formatDate(date) === item.date &&
+                                  'text-red-600 font-bold'
+                                }`}
+                                key={index}
+                              >
+                                <span className="pr-1">[{item.date}]</span>
+                                {item.text}
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        ))}
+        <div className="fixed bottom-0 left-0 p-2 box-border bg-gray-600/40 w-full">
+          {datas.map((data: any, index: any) => (
+            <Link href={`#section-${index}`}>
+              <button
+                type="button"
+                className="px-3 py-1.5 mr-2 text-sm font-semibold leading-6 text-white rounded-md bg-gray-600 hover:bg-gray-800"
+              >
+                {data.depth1}
+              </button>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,7 @@ import transformerVariantGroup from '@unocss/transformer-variant-group';
 
 // 2023-12-12
 // config 정의
-// theme의 colors, fontFamily등 로지소프트 정의 테마를 정의하면 css로 바로 정의 가능 (rules 필요 없음)
+// colors, fontFamily등 로지소프트 테마를 정의하면 css로 바로 적용 가능 (rules 필요 없음)
 // fontsize, padding, margin, zindex, width, height, float등 기본 css는 모두 tailwind css 를 참조
 // tailwind 테마가 있으나 추가된 사이즈, value 값이 없다면 'Arbitrary values' 를 사용해서 추가한다. z-[100]
 // 기본 component와 props에 따른 css는 shortcuts에 추가하여 단일 class 명으로 유지한다.
@@ -12,56 +12,5 @@ import transformerVariantGroup from '@unocss/transformer-variant-group';
 
 export default defineConfig({
   presets: [presetUno()],
-  shortcuts: {
-    // you could still have object style
-    btn: 'py-2 px-4 font-semibold rounded-lg shadow-md',
-    // dynamic shortcuts
-
-    'dropdown-button':
-      'w-38 h-10 p-2 border border-color-#E0E0E0 rounded text-left flex flex-items-center flex-justify-between',
-    'dropdown-arrow': 'transition-all',
-    'dropdown-arrow-active': 'transform-rotate-180',
-    'dropdown-content':
-      'w-38 h-28 mt-1 rounded border border-color-#E0E0E0 p-2 overflow-auto',
-    'dropdown-item':
-      'py-1 pl-1 bg-cover rounded-sm hover-font-500 hover-color-#143B75',
-    submenu: 'float-left mr-5 font-size-5',
-    focused: 'color-#4749fb font-bold border-b-2 border-b-#4749fb',
-  },
-  theme: {
-    colors: {
-      primary: '#00bfa5',
-      secondary: '#2979ff',
-      error: '#ff878d',
-      success: '#4f9a94',
-      gray: {
-        '100': '#cfd9de',
-        '200': '#b0bec5',
-        '300': '#90a4ae',
-        '400': '#78909c',
-        '500': '#607d8b',
-      },
-      status: {
-        good: '#4f9a94',
-        normal: '#5d99c6',
-        danger: '#bb848d',
-        empty: '78909c',
-      },
-    },
-    fontSize: {
-      sm: '0.75rem',
-      base: '1rem',
-      md: '1.25rem',
-      lg: '1.5rem',
-      xl: '1.75rem',
-      xxl: '2rem',
-      xxxl: '2.25rem',
-    },
-    zIndex: {
-      '2': '2',
-      '3': '3',
-    },
-  },
-  rules: [],
   transformers: [transformerVariantGroup()],
 });
